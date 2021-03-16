858a7f55ef4b0897aaba541af496cb17fab14622
unknown <caionsantos2000@gmail.com>
2021-03-16 19:13:45 -0300

    initial extruture swagger

--- app.js
+++ app.js
@@ -1,5 +1,29 @@
 const app = require("./server")
-const aluno = require('./routes');
+const alunoRouter = require('./routes');
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
 
-app.get("/",(req,res)=>res.send("Gloria a Deus"))
-app.use('/aluno', aluno); 
+// Extended: https://swagger.io/specification/#infoObject
+const options = {
+    swaggerDefinition: {
+        openapi: '3.0.0',
+        info: {
+            version: "1.0.0",
+            title: "Customer API",
+            description: "Customer API Information",
+            contact: {
+                name: "Caio Nascimento"
+            },
+            servers: ["http://localhost:8090/aluno"]
+        }
+    },
+    // ['.routes/*.js']
+    apis: ['./routes.js']
+};
+
+const swaggerSpec = swaggerJsDoc(options);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+
+app.get("/", (req, res) => res.send("Gloria a Deus"))
+app.use('/aluno', alunoRouter); 
--- routes.js
+++ routes.js
@@ -1,9 +1,25 @@
 const express = require("express")
-const AlunoController = require("./controllers/alunoController");
 const router = express.Router(); 
+const {index,store} = require("./controllers/alunoController");
 
-// rotas aluno
-router.post('/list', AlunoController.index);
-router.post('/add', AlunoController.store);
+/**
+ * @swagger
+ * /aluno/list:
+ *    post:
+ *      description: Use to return all customers
+ *    parameters:
+ *      - name: customer
+ *        in: query
+ *        description: Name of our customer
+ *        required: false
+ *        schema:
+ *          type: string
+ *          format: string
+ *    responses:
+ *      '201':
+ *        description: Successfully created user
+ */
+router.post('/list', index);
+router.post('/add', store);
 
 module.exports = router 
