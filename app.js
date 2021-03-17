fecd6cab4ec709a6f76d654b9ee3bc438e28a44e
unknown <caionsantos2000@gmail.com>
2021-03-17 09:29:14 -0300

    fix error

--- app.js
+++ app.js
@@ -5,26 +5,6 @@ const swaggerDocument = require('./swagger_output.json');
 
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-// Extended: https://swagger.io/specification/#infoObject
-const options = {
-    definition: {
-        info: {
-          version: "1.0.0",
-          title: "Alunos API",
-          description: "Alunos API Information",
-          contact: {
-            name: "Amazing Developer"
-          },
-          url: ["http://localhost:8090/"]
-        }
-      },
-    // ['.routes/*.js']
-    apis: ['./routes/*.js']
-};
-
-const swaggerSpec = swaggerJsDoc(options);
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-
 
 app.get("/", (req, res) => res.send("Gloria a Deus"))
-app.use('/aluno', alunoRouter); 
+app.use('/aluno', aluno); 
